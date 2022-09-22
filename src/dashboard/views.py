05279419aae77f60086cb89c9241846e0e778c79
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-09-22 13:43:17 +0400

    fix nom template accueil dashboard
    Il y avait une erreur dans le nom du template de la page d'accueil du dashboard

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -5,5 +5,5 @@ from django.template import loader
 # Create your views here.
 def index(request):
     # return HttpResponse("Page dashboard !")
-    template = loader.get_template('dashboard_acceuil.html')
+    template = loader.get_template('dashboard_accueil.html')
     return HttpResponse(template.render())
