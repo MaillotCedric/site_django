0384c57f65ac02d87c2a45d0fca78e176fcb19e4
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-09-22 14:50:34 +0400

    add restriction acces accueil dashboard

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Page dashboard !")
-    template = loader.get_template('dashboard_accueil.html')
-    return HttpResponse(template.render())
+    # template = loader.get_template('dashboard_accueil.html')
+    # return HttpResponse(template.render())
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'dashboard_accueil.html')
