45bf65d6ce80165ecd87c2cb2f87def33ada6c20
DevIA.E15 <DevIA.E15@afpar.com>
2022-09-26 15:38:15 +0400

    update model

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from dashboard.models import Projet, Histo, Threads, Comments
+
+admin.site.register(Projet)
+admin.site.register(Histo)
+admin.site.register(Threads)
+admin.site.register(Comments)
