90f5574fbdd5357c2667d2107a1853decb2c413b
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-09-27 14:32:36 +0400

    Fix url's
    . URL vers dashboard
    . URL de dashboard vers page accueil privée

--- src/home/views.py
+++ src/home/views.py
@@ -5,3 +5,8 @@ def home(request):
     if not request.user.is_authenticated:
         return render(request, 'home/public.html')
     return render(request, 'home/private.html')
+
+def dashboard(request):
+    if not request.user.is_authenticated:
+        return render(request, 'home/public.html')
+    return render(request, 'dashboard_accueil.html')
