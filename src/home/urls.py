693a911ccb34e959830a5c4d1bd848352b12328b
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-09-27 15:49:24 +0400

    feature : accueil dashboard
    . Titre page accueil dashboard dynamique

--- src/home/urls.py
+++ src/home/urls.py
@@ -4,5 +4,5 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('dashboard/', views.dashboard, name="dashboard")
+    path('dashboard/<int:id_projet>', views.dashboard, name="dashboard")
 ]
