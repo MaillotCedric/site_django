af1c11fe9d1cacd18b3fe7af48e730c5cfe5bd01
DevIA.E15 <DevIA.E15@afpar.com>
2022-09-28 10:51:40 +0400

    page priver

--- src/home/views.py
+++ src/home/views.py
@@ -1,7 +1,19 @@
+from tkinter.tix import Select
 from django.shortcuts import render
+from dashboard.models import Histo
+from dashboard.models import Projet
+from django.db.models import Max
 
 # Create your views here.
 def home(request):
+    context = {}
+    context["histos"] = Histo.objects.all()[:10]
+
+    projets = set()
+    for projet in Projet.objects.order_by(): #récupére tous les projets
+        projets.add(Histo.objects.filter(projetId = projet).order_by('-dateRel')[0]) # récupere le dernier histo des ce projet
+    
     if not request.user.is_authenticated:
-        return render(request, 'home/public.html')
-    return render(request, 'home/private.html')
+        return render(request, 'home/public.html', context= context)
+    return render(request, 'home/private.html', {'projets': projets})
+
